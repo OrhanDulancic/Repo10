78b4e1f090c8e891def71c108c23bb39054f13ed
OrhanDulancic <orhan.dulancic@outlook.com>
2022-04-06 17:37:52 +0200

    First object

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+var myPet = {
+ species : "cukac",
+ name : "zlaya",
+ legs : 4,
+ friends : ["Orhan", "Asmir"]
+};
+function myObject(obj) {
+    return obj;
+}
+
+console.log(myObject(myPet));
+
+
